3bab34a30c899fbd88bb227232b7f7b107d6327a
sameer6576 <77893868+sameer6576@users.noreply.github.com>
2024-03-20 15:23:53 +0530

    added dashboard private route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -40,33 +40,33 @@ function Header() {
         >
           {theme === "light" ? <FaMoon /> : <FaSun />}
         </Button>
-        <Link to="/sign-in">
-          {currentUser ? (
-            <Dropdown
-              arrowIcon={false}
-              inline
-              label={
-                <Avatar alt="user" img={currentUser.profilePicture} rounded />
-              }
-            >
-              <Dropdown.Header>
-                <span className="block text-sm">@{currentUser.username}</span>
-                <span className="block text-sm font-medium truncate">
-                  {currentUser.email}
-                </span>
-              </Dropdown.Header>
-              <Link to="/dashboard?tab=profile">
-                <Dropdown.Item>Profile</Dropdown.Item>
-              </Link>
-              <Dropdown.Divider />
-              <Dropdown.Item>Sign Out</Dropdown.Item>
-            </Dropdown>
-          ) : (
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to="/dashboard?tab=profile">
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign Out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
             <Button gradientDuoTone="purpleToBlue" outline>
               Sign In
             </Button>
-          )}
-        </Link>
+          </Link>
+        )}
       </div>
       <Navbar.Toggle />
       <Navbar.Collapse>
