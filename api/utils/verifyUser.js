33f6e4d8feb49dfb87f0f83a86ad64b80020b9c5
sameer6576 <77893868+sameer6576@users.noreply.github.com>
2024-03-21 17:46:27 +0530

    fixed api route verifyUser typo

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,9 +1,7 @@
 import jwt from "jsonwebtoken";
 import { errorHandler } from "./error.js";
-
 export const verifyToken = (req, res, next) => {
-  console.log(req.cookie);
-  const token = req.cookie.access_token;
+  const token = req.cookies.access_token;
   if (!token) {
     return next(errorHandler(401, "Unauthorized"));
   }
