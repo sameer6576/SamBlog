46ad25a8c73d112cfcfdffd4a7c5756cb65c3b45
sameer6576 <77893868+sameer6576@users.noreply.github.com>
2024-03-21 17:45:43 +0530

    added api route

--- api/index.js
+++ api/index.js
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 import { connnectDB } from "./db/connectDB.js";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 connnectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
---
+++ api/utils/verifyUser.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import { errorHandler } from "./error.js";
+
+export const verifyToken = (req, res, next) => {
+  console.log(req.cookie);
+  const token = req.cookie.access_token;
+  if (!token) {
+    return next(errorHandler(401, "Unauthorized"));
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return next(errorHandler(401, "Unauthorized"));
+    }
+    req.user = user;
+    next();
+  });
+};
